03bcb6b1f6389052ba036a8d9cc4161215b55e26
wom2000 <costafabiana970@gmail.com>
2026-01-26 12:25:58 +0000

    controllers

--- cursos/routes/auth.php
+++ cursos/routes/auth.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -56,4 +57,32 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+});
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // CURSOS - ADICIONA AQUI
+    Route::get('/cursos', function () {
+        return Inertia::render('Courses/AllCourses');
+    })->name('cursos.index');
+
+    // CATEGORIAS
+    Route::get('/categorias', function () {
+        return Inertia::render('Categorias/Index');
+    })->name('categorias.index');
+
+    // MATERIAIS
+    Route::get('/materiais', function () {
+        return Inertia::render('Materiais/Index');
+    })->name('materiais.index');
 });
