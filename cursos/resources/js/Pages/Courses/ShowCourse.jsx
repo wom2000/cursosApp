dd0e80711da03fb0c0aaf6097f3172ac3c1d8c74
wom2000 <costafabiana970@gmail.com>
2026-02-04 14:30:02 +0000

    x

--- cursos/resources/js/Pages/Courses/ShowCourse.jsx
+++ cursos/resources/js/Pages/Courses/ShowCourse.jsx
@@ -1,5 +1,4 @@
 import MainLayout from "@/Layouts/MainLayout";
-<<<<<<< angeloBranch
 import { Head } from "@inertiajs/react";
 import "../../../css/ShowCourse.css";
 
@@ -59,29 +58,6 @@ export default function ShowCourse({ auth, course }) {
                             </li>
                         ))}
                     </ul>
-=======
-import { Head, Link } from "@inertiajs/react";
-
-export default function ShowCourse({ auth, id }) {
-    return (
-        <MainLayout user={auth.user}>
-            <Head title="Curso" />
-            <div className="max-w-4xl mx-auto px-6 py-10">
-                <h1 className="text-2xl font-semibold text-gray-900">
-                    Detalhes do Curso
-                </h1>
-                <p className="mt-2 text-gray-600">
-                    Aceda aos materiais e registe o seu progresso.
-                </p>
-
-                <div className="mt-6">
-                    <Link
-                        href={`/curso/${id}/materiais`}
-                        className="inline-flex items-center gap-2 rounded-lg bg-black px-4 py-2 text-sm font-semibold text-white transition hover:bg-gray-800"
-                    >
-                        Ver Materiais
-                    </Link>
->>>>>>> main
                 </div>
             </div>
         </MainLayout>
